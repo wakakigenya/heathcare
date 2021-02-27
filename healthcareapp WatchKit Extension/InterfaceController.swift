d7f41f9f086017a8efa6e7d15ab4cc5dd8ac0b75
genya.wakaki <wakawakawaka44@gmail.com>
2021-02-27 14:29:13 +0900

    Initial Commit

---
+++ healthcareapp WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  healthcareapp WatchKit Extension
+//
+//  Created by 若木弦也 on 2021/02/27.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
---
+++ healthcareapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  healthcareapp
+//
+//  Created by 若木弦也 on 2021/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
